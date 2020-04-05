2cf2701d3b2e7c15f199795f6145a348c29df9cb
jalak <jalakpatoliya777@gmail.com>
2020-04-05 11:00:54 +0530

    Updates

---
+++ precache-manifest.daccecce54a0a22f961c136bfa9b5964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8560ec178545fe2b310d1411a532ca91",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eadb39a5469e7fe9cece",
+    "url": "/monsters-rolodex/static/css/main.0790b8cd.chunk.css"
+  },
+  {
+    "revision": "8fa26bdceea933a7924b",
+    "url": "/monsters-rolodex/static/js/2.e9ffff92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e9ffff92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadb39a5469e7fe9cece",
+    "url": "/monsters-rolodex/static/js/main.4bd9d4cd.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
